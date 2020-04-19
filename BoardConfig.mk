eb3d46bff8b636775e65d5f179e1f39030cf903a
XRed CubeX <xred_cubex@hotmail.com>
2020-04-19 14:55:46 +0200

    Switch to Android-10

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,10 +38,11 @@ BOARD_MKBOOTIMG_ARGS += \
 # Partitons
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 50331648
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16000000
+TARGET_USERIMAGES_USE_F2FS := true
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/root/etc/twrp.stock
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/root/etc/twrp.treble
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 BOARD_HAS_NO_SELECT_BUTTON := true
@@ -50,16 +51,18 @@ TW_EXCLUDE_TWRPAPP := true
 TW_INCLUDE_NTFS_3G := true
 TW_INCLUDE_CRYPTO := true
 TW_THEME := portrait_hdpi
+TW_MAX_BRIGHTNESS := 255
 
 # MultiROM config
 TARGET_RECOVERY_IS_MULTIROM := true
 MR_DEVICE_HAS_VENDOR_PARTITION := true
+MR_FIRMWARE_DIR := "/vendor/firmware"
 MR_INPUT_TYPE := type_b
 MR_INIT_DEVICES := device/meizu/mblu2/multirom/mr_init_devices.c
 MR_RD_ADDR := 0x4F88000
 MR_DPI := hdpi
 MR_DPI_FONT := 216
-MR_FSTAB := device/meizu/mblu2/recovery/root/etc/twrp.stock
+MR_FSTAB := device/meizu/mblu2/recovery/root/etc/twrp.treble
 MR_USE_MROM_FSTAB := true
 MR_DEVICE_BOOTDEVICE := /dev/block/platform/mtk-msdc.0/11230000.msdc0
 MR_NO_KEXEC := enabled
