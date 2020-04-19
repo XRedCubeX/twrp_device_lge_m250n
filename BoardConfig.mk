1a96f8baa81ed03f429ce2936e9af47bc1c35bff
XRed CubeX <xred_cubex@hotmail.com>
2020-04-19 15:34:40 +0200

    Add a Hook

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,7 +7,7 @@ TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_VARIANT := cortex-a53
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
@@ -38,10 +38,11 @@ BOARD_MKBOOTIMG_ARGS += \
 # Partitons
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 50331648
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16000000
+TARGET_USERIMAGES_USE_F2FS := true
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/root/etc/twrp.stock
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/root/etc/twrp.treble
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 BOARD_HAS_NO_SELECT_BUTTON := true
@@ -54,12 +55,14 @@ TW_THEME := portrait_hdpi
 # MultiROM config
 TARGET_RECOVERY_IS_MULTIROM := true
 MR_DEVICE_HAS_VENDOR_PARTITION := true
+#MR_DEVICE_HOOKS := $(PLATFORM_PATH)/multirom/mr_hooks.c
+#MR_DEVICE_HOOKS_VER := 5
 MR_INPUT_TYPE := type_b
 MR_INIT_DEVICES := device/meizu/mblu2/multirom/mr_init_devices.c
 MR_RD_ADDR := 0x4F88000
 MR_DPI := hdpi
 MR_DPI_FONT := 216
-MR_FSTAB := device/meizu/mblu2/recovery/root/etc/twrp.stock
+MR_FSTAB := device/meizu/mblu2/recovery/root/etc/twrp.treble
 MR_USE_MROM_FSTAB := true
 MR_DEVICE_BOOTDEVICE := /dev/block/platform/mtk-msdc.0/11230000.msdc0
 MR_NO_KEXEC := enabled
